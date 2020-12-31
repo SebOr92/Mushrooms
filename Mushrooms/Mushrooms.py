15068473693bbb9bbd49c091922d42b2db5baf9d
SebastianO <SOrth@omniga.de>
2020-12-31 14:28:47 +0100

    Edited readme and added confusion matrix"

--- Mushrooms/Mushrooms.py
+++ Mushrooms/Mushrooms.py
@@ -6,6 +6,7 @@ matplotlib.style.use('ggplot')
 from tpot import TPOTClassifier
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 from sklearn.model_selection import train_test_split, RepeatedStratifiedKFold
+from sklearn.metrics import confusion_matrix,accuracy_score
 
 def load_and_check_data(path):
 
@@ -31,7 +32,7 @@ def exploratory_data_analysis(data, plot_with_target = False, save = False):
             plt.savefig(fname=str(col)+"_count.png")
         plt.show()
 
-def prepocess_data(data, test_size, seed):
+def prepocess_data(data, test_size, seed, save = False):
     X = data[[x for x in list(data.columns) if x not in ["class"]]]
     y = data["class"]
 
@@ -53,13 +54,36 @@ def prepocess_data(data, test_size, seed):
     return X_train, X_test, y_train, y_test
 
 def TPOT(generations, population_size, cv, seed, verbosity):
+
     pipeline_optimizer = TPOTClassifier(generations=generations,
                                         population_size=population_size,
                                         cv=cv,
                                         random_state=seed, 
                                         verbosity=verbosity)
+
     pipeline_optimizer.fit(X_train, y_train)
-    pipeline_optimizer.score(X_test, y_test)
+    y_true, y_pred= y_test, model.predict(X_test)
+    
+    acc = accuracy_score(y_true, y_pred)
+    print("Accuracy on test set: " + str(bal_acc))
+
+    cm = confusion_matrix(y_true, y_pred)
+    ax = sns.heatmap(cm,
+                 annot=True,
+                 fmt="d",
+                 cbar=False,
+                 cmap="Blues",
+                 xticklabels=['Edible', 'Poisonous'],
+                 yticklabels=['Edible', 'Poisonous'])
+    bottom, top = ax.get_ylim()
+    ax.set_ylim(bottom + 0.5, top - 0.5)
+    plt.xlabel('Predicted Labels')
+    plt.ylabel('True Labels')
+    plt.title('Confusion Matrix test set')
+    if save:
+        plt.savefig(fname="ConfusionMatrix.png")
+    plt.show()
+
     pipeline_optimizer.export('tpot_exported_pipeline.py')
 
 random_seed = 191
@@ -67,4 +91,4 @@ data = load_and_check_data('mushrooms.csv')
 #exploratory_data_analysis(data, True, True)
 X_train, X_test, y_train, y_test = prepocess_data(data, 0.25, random_seed)
 cv = RepeatedStratifiedKFold(n_splits=5, n_repeats=3, random_state=random_seed)
-TPOT(3, 10, cv, random_seed, 3)
+TPOT(3, 10, cv, random_seed, 3, False)
